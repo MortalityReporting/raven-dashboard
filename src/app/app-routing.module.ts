16e2b53fb3203fe7fec3979a7e764f1b28f55eff
ptassev3 <ptassev3@gatech.edu>
2022-01-31 14:14:07 -0500

    basic form for FHIR validator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FhirValidatorComponent} from "./components/pages/fhir-validator/fhir-validator.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FhirValidatorComponent
+  },
+  { // Do not add any paths below this point, this path MUST ALWAYS be the last path!
+    path: '**', redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FhirValidatorComponent } from './components/pages/fhir-validator/fhir-validator.component';
+import { HeaderComponent } from './components/header/header.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FhirValidatorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
