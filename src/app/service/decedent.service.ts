0bdeda7dc47b83aba787f03bbd55abdb8dcd0a75
Plamen Tassev <plamen@gatech.edu>
2022-02-14 09:09:21 -0500

    Working code, but needs a lot of clean up. Lods records conceptually, but not the way we want to use rxjs or records in general.

--- src/app/service/decedent.service.ts
+++ src/app/service/decedent.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams, HttpErrorResponse} from "@angular/common/http";
+import {HttpClient, HttpParams, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable, throwError} from "rxjs";
 import {map} from "rxjs/operators";
 import {DecedentResponse} from "../model/decedent.response";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -24,20 +25,52 @@ export class DecedentService {
     const pageNumberParam: number = pageNumber || 0;
     const pageSizePram: number = pageSize || 10;
 
-    return this.http.get('/api/decedent-list', {
-      params: new HttpParams()
-        .set('filter', filterParam)
-        .set('sortBy', sortByParam)
-        .set('sortOrder', sortOrderParam)
-        .set('pageNumber', pageNumberParam)
-        .set('pageSize', pageSizePram)
-    }).pipe( map((result: any) => (
-        result as DecedentResponse
-      )),
-      catchError((error: HttpErrorResponse) => throwError("The system was unable to generate new test: "
-        + error.status + ". Report this error if it persists."
-      ))
+    const headers_object = new HttpHeaders();
+    headers_object.append('Content-Type', 'application/json');
+    headers_object.append("Authorization", "Basic " + btoa("client:secret"));
+
+    const httpOptions = {
+      headers: headers_object
+    };
+
+    return this.http.get("https://apps.hdap.gatech.edu/raven-fhir-server/fhir/Patient", httpOptions).pipe( map((result: any) =>
+        {console.log (result); return result as DecedentResponse}
+      )
     );
   }
 
+
+  testCall():  Observable<any> {
+
+    let authorizationData = 'Basic ' + btoa('client' + ':' + 'secret');
+
+    const headerOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': authorizationData
+      })
+    };
+
+
+    return this.http.get("https://apps.hdap.gatech.edu/raven-fhir-server/fhir/Patient", headerOptions).pipe( map((result: any) =>
+        {return result}
+      )
+    );
+  }
+
+  getDetails(patient: any):  Observable<any> {
+    let authorizationData = 'Basic ' + btoa('client' + ':' + 'secret');
+
+    const headerOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': authorizationData
+      })
+    };
+
+    return this.http.get("https://apps.hdap.gatech.edu/raven-fhir-server/fhir/Observation?patient=" + patient.resource.id + "&code=81956-5", headerOptions).pipe( map((result: any) =>
+        { return result?.entry[0]?.resource?.effectiveDateTime}
+      )
+    );
+  }
 }
