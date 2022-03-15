c86bb3ec9d4507ee34fe885680c532ccc1d351fc
Plamen Tassev <plamen@gatech.edu>
2022-03-15 16:37:12 -0400

    FHIR validator commit at stopping point

--- src/app/components/pages/fhir-validator-js/fhir-validator-js.component.ts
+++ src/app/components/pages/fhir-validator-js/fhir-validator-js.component.ts
@@ -1,22 +1,52 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewEncapsulation} from '@angular/core';
 import {UtilsService} from "../../../service/utils.service";
 import {FhirValidatorService} from "../../../service/fhir-validator.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-fhir-validator-js',
   templateUrl: './fhir-validator-js.component.html',
   styleUrls: ['./fhir-validator-js.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class FhirValidatorJsComponent implements OnInit {
   fhirResource: string ='';
   resourceFormat = 'json';
   fileName: string;
   validationErrorStr: string;
-  isValidErrorMsgRendered = false;
+  isErrorMsgRendered = false;
   isFormattingPerformedRendered = false;
   isValidResourceMsgRendered = false;
-  parsedFhirResource: string= '';
+  hasBackendValidationErrors = false;
+  parsedFhirResource : any;
+  displayedColumns: string[] = ['severity', 'fhirPath', 'message', 'location'];
+  apiErrorResponse = [
+    {
+      "severity": "Warning",
+      "fhirPath": "Patient.address[0].state",
+      "location": "(line 4, col24)",
+      "message": "The value provided ('Texas') is not in the value set http://hl7.org/fhir/us/core/ValueSet/us-core-usps-state (http://hl7.org/fhir/us/core/ValueSet/us-core-usps-state), and a code should come from this value set unless it has no suitable code (note that the validator cannot judge what is suitable)  (error message = The code \"Texas\" is not valid in the system https://www.usps.com/; The code provided (https://www.usps.com/#Texas) is not valid in the value set 'UspsTwoLetterAlphabeticCodes' (from http://tx.fhir.org/r4))\r\n"
+    },
+    {
+      "severity": "Warning",
+      "fhirPath": "Patient.address[0].use",
+      "location": "(line 6, col6)",
+      "message": "ValueSet http://hl7.org/fhir/ValueSet/address-use|4.0.1 not found by validator"
+    },
+    {
+      "severity": "Error",
+      "fhirPath": "Patient.identifier[0].type",
+      "location": "(line 9, col20)",
+      "message": "None of the codings provided are in the value set http://hl7.org/fhir/ValueSet/identifier-type (http://hl7.org/fhir/ValueSet/identifier-type), and a coding should come from this value set unless it has no suitable code (note that the validator cannot judge what is suitable) (codes = http://cbsig.chai.gatech.edu/CodeSystem/cbs-temp-code-system#Local-Record-ID)"
+    },
+    {
+      "severity": "Error",
+      "fhirPath": "Patient.identifier[0].type",
+      "location": "(line 17, col20)",
+      "message": "None of the codings provided are in the value set http://hl7.org/fhir/ValueSet/identifier-type (http://hl7.org/fhir/ValueSet/identifier-type), and a coding should come from this value set unless it has no suitable code (note that the validator cannot judge what is suitable) (codes = http://cbsig.chai.gatech.edu/CodeSystem/cbs-temp-code-system#Local-Record-ID)"
+    }
+  ];
 
   response = {
       "resourceType" : "Observation",
@@ -51,6 +81,7 @@ export class FhirValidatorJsComponent implements OnInit {
     private utilsService: UtilsService,
     private fhirValidatorService: FhirValidatorService,
     private _snackBar: MatSnackBar,
+    private sanitized: DomSanitizer
   ) { }
 
   // It is important the format is working with "best effort"
@@ -87,7 +118,7 @@ export class FhirValidatorJsComponent implements OnInit {
     this.fileName=''
     this.validationErrorStr = '';
     this.isFormattingPerformedRendered = false;
-    this.isValidErrorMsgRendered = false;
+    this.isErrorMsgRendered = false;
     this.isValidResourceMsgRendered = false;
   }
 
@@ -137,30 +168,17 @@ export class FhirValidatorJsComponent implements OnInit {
   }
 
   validateFhirResource(fhirResource: any, resourceFormat: string) {
-    const lines = this.fhirResource.split('\n');
-    console.log(lines);
-    for (let i = 0; i < lines.length; i++) {
-      // let line = i++;
-      if (i == 2) {
-        let tempText = '<mark>' + lines[i] + '</mark>';
-        this.parsedFhirResource += tempText;
-        this.parsedFhirResource += '\n';
-      } else {
-        this.parsedFhirResource += lines[i];
-        this.parsedFhirResource += '\n';
-      }
+
+    this.validationErrorStr = this.fhirValidatorService.getUiValidationMessages(fhirResource, resourceFormat);
+    if(this.validationErrorStr){
+      //see if we can find any obvious issues with the resource here
+      this.isErrorMsgRendered = true;
+      this.isValidResourceMsgRendered = false;
     }
-    console.log(this.parsedFhirResource);
-    // this.validationErrorStr = this.fhirValidatorService.getUiValidationMessages(fhirResource, resourceFormat);
-    // if(this.validationErrorStr){
-    //   this.isValidErrorMsgRendered = true;
-    //   this.isValidResourceMsgRendered = false;
-    // }
-    // else {
-    //   this.isValidErrorMsgRendered = false;
-    //   this.isValidResourceMsgRendered = true;
-    // }
-    // this.fhirResource = this.parsedFhirResource
+    else {
+      // The UI validation passed successfully, and we execute the backend validation.
+      this.executeAPIValidation(fhirResource, resourceFormat);
+    }
   }
 
   onPasteFhirResource(event: ClipboardEvent) {
@@ -175,5 +193,83 @@ export class FhirValidatorJsComponent implements OnInit {
         this.resourceFormat = 'xml';
       }
     }
+  }
+
+  getErrorLineNumbers(apiResponse: any): number[]{
+    if(!apiResponse || apiResponse.length === 0){
+      return null;
+    }
+    return [9, 17];
+  };
+
+  getWarningWarningLineNumber(apiResponse: any): number[]{
+    if(!apiResponse || apiResponse.length === 0){
+      return null;
+    }
+    return [4, 6];
+  };
+
+  escapeHtml(str: string): string {
+    // We escape all html tags in order to render the html as text in the innerHTML div
+    return str
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
+
+  renderAPIValidationErrors() {
+
+    const errorLineNumbers = this.getErrorLineNumbers(this.apiErrorResponse);
+    const warningLineNumbers = this.getWarningWarningLineNumber(this.apiErrorResponse);
+
+    if(errorLineNumbers?.length > 0 || warningLineNumbers?.length > 0){
+      this.hasBackendValidationErrors = true;
+      this.validationErrorStr = "Please see the validation errors below.";
+      this.isErrorMsgRendered = true;
+    }
+
+    const lines = this.fhirResource.split('\n');
+    lines.forEach((line, i) => {
+      const sanitized = this.escapeHtml(line);
+      if(!this.parsedFhirResource){
+        this.parsedFhirResource = '';
+      }
+      if(errorLineNumbers.indexOf(i) != -1){
+        let tempText = '<span class="error-mark" id="error' + i + '">' + sanitized + '</span>';
+        this.parsedFhirResource += tempText;
+        this.parsedFhirResource += '\n';
+      }
+      else if(warningLineNumbers.indexOf(i) != -1){
+        let tempText = '<span class="warning-mark" id="warning' + i + '">' + sanitized + '</span>';
+        this.parsedFhirResource += tempText;
+        this.parsedFhirResource += '\n';
+      }
+      else {
+        this.parsedFhirResource += sanitized;
+        this.parsedFhirResource += '\n';
+      }
+    });
+    this.parsedFhirResource = this.sanitized.bypassSecurityTrustHtml(this.parsedFhirResource);
+    if(!this.hasBackendValidationErrors){
+      this.isValidResourceMsgRendered = true;
+    }
+  }
+
+  scrollToElement(location: string ): void {
+    const element = document.querySelector(location);
+    if (element) element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+  }
+
+  onLocationSelected(response: any): void {
+    let locationId = ('#'+ response.severity + response.location.split(",")[0].replace( /^\D+/g, '')).toLowerCase();
+    this.scrollToElement(locationId);
+  }
+
+  private executeAPIValidation(fhirResource: any, resourceFormat: string) {
+    this.fhirValidatorService.validateFhirResource(fhirResource, resourceFormat).subscribe((response: any) => {
+      console.log(response);
+    })
   }
 }
--- src/app/service/fhir-validator.service.ts
+++ src/app/service/fhir-validator.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import {UtilsService} from "./utils.service";
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FhirValidatorService {
 
-  constructor( private utilsService: UtilsService) { }
+  constructor( private utilsService: UtilsService, private http: HttpClient) { }
 
   getUiValidationMessages(fhirResource: any, resourceFormat: string): string {
 
@@ -17,7 +20,7 @@ export class FhirValidatorService {
       if(!this.utilsService.isJsonString(fhirResource)){
         return "Invalid json format detected.";
       }
-      else if(!fhirResource.resourceType){
+      else if(!JSON.parse(fhirResource).resourceType){
         return "Missing required resourceType property.";
       }
     }
@@ -26,4 +29,34 @@ export class FhirValidatorService {
     }
     return null;
   }
+
+  validateFhirResource(fhirResource: any, resourceFormat: string):  Observable<any> {
+
+    const testPostCall = {
+      "resourceType": "Parameters",
+      "parameter": [
+        {
+          "name": "ig",
+          "valueString": "hl7.fhir.us.mdi#current"
+        },
+        {
+          "name": "profile",
+          "valueString": "http://hl7.org/fhir/us/mdi/StructureDefinition/Observation-death-date"
+        },
+        {
+          "name": "format",
+          "valueString": "json"
+        },
+        {
+          "name": "sourceContent",
+          "valueString": "%7B%0A%20%20%22resourceType%22%20%3A%20%22Observation%22%2C%0A%20%20%22id%22%20%3A%20%22observation-death-date-j-rogers%22%2C%0A%20%20%22meta%22%20%3A%20%7B%0A%20%20%20%20%22versionId%22%20%3A%20%221%22%2C%0A%20%20%20%20%22lastUpdated%22%20%3A%20%222022-02-17T03%3A30%3A31.175%2B00%3A00%22%2C%0A%20%20%20%20%22source%22%20%3A%20%22%23HxNQbdXHR9YLhjG8%22%2C%0A%20%20%20%20%22profile%22%20%3A%20%5B%0A%20%20%20%20%20%20%22http%3A%2F%2Fhl7.org%2Ffhir%2Fus%2Fmdi%2FStructureDefinition%2FObservation-death-date%22%0A%20%20%20%20%5D%0A%20%20%7D%2C%0A%20%20%22status%22%20%3A%20%22final%22%2C%0A%20%20%22component%22%20%3A%20%5B%0A%20%20%20%20%7B%0A%20%20%20%20%20%20%22code%22%20%3A%20%7B%0A%20%20%20%20%20%20%20%20%22coding%22%20%3A%20%5B%0A%20%20%20%20%20%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%22system%22%20%3A%20%22http%3A%2F%2Floinc.org%22%2C%0A%20%20%20%20%20%20%20%20%20%20%20%20%22code%22%20%3A%20%2280616-6%22%2C%0A%20%20%20%20%20%20%20%20%20%20%20%20%22display%22%20%3A%20%22Date%20and%20time%20pronounced%20dead%20%5BUS%20Standard%20Certificate%20of%20Death%5D%22%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%5D%0A%20%20%20%20%20%20%7D%2C%0A%20%20%20%20%20%20%22valueDateTime%22%20%3A%20%222022-01-04T05%3A30%3A00-05%3A00%22%0A%20%20%20%20%7D%0A%20%20%5D%0A%7D"
+        }
+      ]
+    }
+
+    return this.http.post("https://gt-apps.hdap.gatech.edu/HL7ValidatorService/fhir/Bundle/$validate", testPostCall).pipe( map((result: any) => (
+      result as Object
+    )));
+  }
+
 }
