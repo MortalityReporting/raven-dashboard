f837ae42957ea55ef6da65186ec4028b9f6e2020
Plamen Tassev <plamen@gatech.edu>
2022-08-04 11:39:24 -0400

    Continued work on import case from a template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { MatFormFieldModule} from "@angular/material/form-field";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from '@angular/material/button';
@@ -15,7 +15,7 @@ import {DecedentRecordsGridComponent} from './components/pages/decedent-records-
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatSortModule} from "@angular/material/sort";
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatCardModule} from "@angular/material/card";
@@ -23,21 +23,30 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
 import {FhirValidatorModule} from "./fhir-validator/fhir-validator.module";
 import {FhirAuthInterceptor} from "./interceptors/fhir-auth.interceptor";
 import {MatGridListModule} from '@angular/material/grid-list';
-import { CaseSummaryComponent } from './components/pages/case-summary/case-summary.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { CaseContainerComponent } from './components/pages/case-container/case-container.component';
+import {CaseSummaryComponent} from './components/pages/case-summary/case-summary.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {CaseContainerComponent} from './components/pages/case-container/case-container.component';
 import {FhirExplorerDrawerService} from "./service/fhir-explorer-drawer.service";
 import {MatExpansionModule} from '@angular/material/expansion';
-import { FhirExplorerComponent } from './components/fhir-explorer/fhir-explorer.component';
+import {FhirExplorerComponent} from './components/fhir-explorer/fhir-explorer.component';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
-import { CaseSummaryContentComponent } from './components/pages/case-summary/case-summary-content/case-summary-content.component';
-import { SetFhirExplorerDirective } from './directives/set-fhir-explorer.directive';
+import {
+  CaseSummaryContentComponent
+} from './components/pages/case-summary/case-summary-content/case-summary-content.component';
+import {SetFhirExplorerDirective} from './directives/set-fhir-explorer.directive';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import { CaseSummaryContentFieldComponent } from './components/pages/case-summary/case-summary-content-field/case-summary-content-field.component';
+import {
+  CaseSummaryContentFieldComponent
+} from './components/pages/case-summary/case-summary-content-field/case-summary-content-field.component';
 import {MatListModule} from "@angular/material/list";
-import { ImportCaseComponent } from './components/pages/import-case/import-case.component';
-import { ImportCaseFhirRecordComponent } from './components/widgets/import-case-fhir-record/import-case-fhir-record.component';
-import { ImportCaseConnectathonTemplateComponent } from './components/widgets/import-case-connectathon-template/import-case-connectathon-template.component';
+import {ImportCaseComponent} from './components/pages/import-case/import-case.component';
+import {
+  ImportCaseFhirRecordComponent
+} from './components/widgets/import-case-fhir-record/import-case-fhir-record.component';
+import {
+  ImportCaseConnectathonTemplateComponent
+} from './components/widgets/import-case-connectathon-template/import-case-connectathon-template.component';
+import {MatTabsModule} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
@@ -55,37 +64,39 @@ import { ImportCaseConnectathonTemplateComponent } from './components/widgets/im
     ImportCaseFhirRecordComponent,
     ImportCaseConnectathonTemplateComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatProgressSpinnerModule,
-        MatSortModule,
-        HttpClientModule,
-        MatRadioModule,
-        FormsModule,
-        MatCardModule,
-        MatSnackBarModule,
-        FhirValidatorModule,
-        MatGridListModule,
-        MatExpansionModule,
-        MatButtonToggleModule,
-        MatTooltipModule,
-        MatListModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    HttpClientModule,
+    MatRadioModule,
+    FormsModule,
+    MatCardModule,
+    MatSnackBarModule,
+    FhirValidatorModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatTooltipModule,
+    MatListModule,
+    MatTabsModule,
+  ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: FhirAuthInterceptor, multi: true },
+    {provide: HTTP_INTERCEPTORS, useClass: FhirAuthInterceptor, multi: true},
     FhirExplorerDrawerService
-    ],
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/widgets/import-case-connectathon-template/import-case-connectathon-template.component.ts
+++ src/app/components/widgets/import-case-connectathon-template/import-case-connectathon-template.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {ImportCaseService} from "../../../service/import-case.service";
 import {UtilsService} from "../../../service/utils.service";
 
+
+//TODO: Remove this code. It is only for testing the table.
+const CASE_DATA = [
+  {id: 1, name: "George Burdell", status: "Complete"},
+  {id: 2, name: "Georgia Burdell", status: "Complete"},
+  {id: 3, name: "John Doe", status: "Error"},
+  {id: 4, name: "Jane Doe", status: "Error"},
+  {id: 5, name: "Richard Smith", status: "Complete"},
+]
+
 @Component({
   selector: 'app-import-case-connectathon-template',
   templateUrl: './import-case-connectathon-template.component.html',
@@ -12,6 +22,9 @@ export class ImportCaseConnectathonTemplateComponent implements OnInit {
   isLoading: boolean = false;
   file: File = null;
   MAX_FILE_SIZE = 100000; // Max allowed file size is 100KB
+  displayedColumns: string[] = ['id', 'name', 'status'];
+  dataSource = CASE_DATA;
+  caseNarrative: string = `<div><div class=\"hapiHeaderText\"> Achim-Uwe <b>BERGMANNSBERG </b></div><table class=\"hapiPropertyTable\"><tbody><tr><td>Identifier</td><td>Pat-473</td></tr><tr><td>Address</td><td><span>Luebeck </span></td></tr><tr><td>Date of birth</td><td><span>04 June 1802</span></td></tr></tbody></table></div>`;
 
   constructor(private importCaseService: ImportCaseService,
               private utilsService: UtilsService) { }
