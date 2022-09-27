f11fa97ae10deb7274fd94871e41d6487c35aa6d
Ellie Shivers <elliemshivers@gmail.com>
2022-09-27 14:35:59 -0400

    Naming Convention Change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'raven2-dashboard'`, () => {
+  it(`should have as title 'raven'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('raven2-dashboard');
+    expect(app.title).toEqual('raven');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('raven2-dashboard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('raven app is running!');
   });
 });
