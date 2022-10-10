c80748ceeea4ddff04f0718c2ab9b6a2db5daa9c
Plamen Tassev <plamen@gatech.edu>
2022-10-10 17:39:17 -0400

    Added code to render the results from the Export to Excel operation.

--- src/app/components/pages/import-case/import-case-connectathon-template/import-case-connectathon-template.component.ts
+++ src/app/components/pages/import-case/import-case-connectathon-template/import-case-connectathon-template.component.ts
@@ -17,6 +17,8 @@ export class ImportCaseConnectathonTemplateComponent implements OnInit {
   MAX_FILE_SIZE = 100000; // Max allowed file size is 100KB
   displayedColumns: string[] = ['name', 'status', 'state'];
   dataSource = new MatTableDataSource<any>();
+  isExportSuccessful: boolean = false;
+  errorsGenerated: boolean = false;
 
   constructor(private importCaseService: ImportCaseService,
               private utilsService: UtilsService,
@@ -45,19 +47,10 @@ export class ImportCaseConnectathonTemplateComponent implements OnInit {
 
   onSubmit() {
     if(this.file) {
+      this.isExportSuccessful = false;
+      this.errorsGenerated = false;
       this.isLoading = true;
-      // this.importCaseService.uploadFile(this.file).subscribe({
-      //   next: value => {
-      //     this.isLoading = false
-      //   },
-      //   error: err => {
-      //     console.error(err);
-      //     this.isLoading = false;
-      //     this.utilsService.showErrorMessage("Error uploading file " + this.file?.name);
-      //     this.file = null;
-      //   }
-      // });
-      this.importCaseService.getMockResponse().subscribe({
+      this.importCaseService.uploadFile(this.file).subscribe({
         next: value => {
           this.isLoading = false;
           console.log(value);
@@ -65,12 +58,14 @@ export class ImportCaseConnectathonTemplateComponent implements OnInit {
           if(value?.length > 0){
             this.selectedCase = value[0];
           }
+          this.isExportSuccessful = true;
         },
         error: err => {
           console.error(err);
           this.isLoading = false;
           this.utilsService.showErrorMessage("Error uploading file " + this.file?.name);
           this.file = null;
+          this.errorsGenerated = true;
         }
       });
     }
--- src/app/components/pages/import-case/mappings/mappings.component.ts
+++ src/app/components/pages/import-case/mappings/mappings.component.ts
@@ -11,7 +11,9 @@ export class MappingsComponent implements OnInit, OnChanges {
   @Input() fhirBundle: any;
   displayedColumns: string[] = ['name', 'value', 'status'];
   dataSource = new MatTableDataSource<any>();
-
+  selectedFilter: string = 'Show all';
+  filters: string[] = ['Show all', 'Show Mapped', 'Show Unmapped'];
+  mappedData: any[];
   constructor() { }
 
   ngOnInit(): void {
@@ -20,14 +22,29 @@ export class MappingsComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['fhirBundle'].currentValue){
       const value = changes['fhirBundle'].currentValue;
-      const mapped = Object.keys(value.fields).map(key => {return {
+      this.mappedData = Object.keys(value.fields).map(key => {return {
         name: key,
         status: value.fields[key].status,
         value: value.fields[key].value,
         FHIRResource: value.fields[key].FHIRResource,
-      }});
-      this.dataSource.data = mapped;
+      }})
+        .sort((a,b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1))
+        .sort((a,b) => (a.status.toLowerCase() > b.status.toLowerCase() ? -1 : 1));
+      this.dataSource.data = this.mappedData;
     }
   }
 
+  onFilterChanged(filter: string) {
+    if(this.mappedData?.length > 0) {
+      if (filter === this.filters[0]) {
+        this.dataSource.data = this.mappedData;
+      }
+      else if (filter === this.filters[1]) {
+        this.dataSource.data = this.mappedData.filter(element => element.status === "mapped");
+      }
+      else if (filter === this.filters[2]) {
+        this.dataSource.data = this.mappedData.filter(value => value.status === "not mapped")
+      }
+    }
+  }
 }
