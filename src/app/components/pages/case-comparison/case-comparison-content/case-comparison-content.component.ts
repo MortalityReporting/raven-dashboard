acb8087dd515e2232556e703f0622b87bf9f63b8
Russell Mitchell <russell.mitchell@gtri.gatech.edu>
2022-10-13 16:54:13 -0400

    Added cap to select the expected resource composition

--- src/app/components/pages/case-comparison/case-comparison-content/case-comparison-content.component.ts
+++ src/app/components/pages/case-comparison/case-comparison-content/case-comparison-content.component.ts
@@ -12,6 +12,7 @@ import { ObservationMannerOfDeathDiff } from './models/observation-manner-of-dea
 import { USCorePractitionerDiff } from './models/us-core-practitioner.diff';
 import { ObservationCauseOfDeathPart1Diff } from './models/observation-cause-of-death-part-1.diff';
 import { ObservationCauseOfDeathPart2Diff } from './models/observation-cause-of-death-part-2.diff';
+import { DecedentService } from "../../../../service/decedent.service";
 import { 
   Comp_MDItoEDRS, 
   Obs_CauseOfDeathPart1, 
@@ -24,6 +25,7 @@ import {
   USCorePatient, 
   USCorePractitioner
 } from "../../../../model/mdi/profile.list"
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-case-comparison-content',
@@ -33,6 +35,15 @@ import {
 export class CaseComparisonContentComponent implements OnInit {
   @ViewChild(MatAccordion) accordion: MatAccordion;
   
+  testCases = [
+    {"compositionId": "c926f394-e442-4135-92bc-1e70e8a09b91", "display": "Whago C Brox"},
+    {"compositionId": "358a3a7d-6011-463c-ad59-0077ad482b64", "display": "Brenda Estrat"},
+    {"compositionId": "f6651a25-0296-4de0-8bd0-00d720607e1b", "display": "Erica Stevens"}, 
+    {"compositionId": "c926f394-e442-4135-92bc-1e70e8a09b91", "display": "Whago C Brox"},
+  ]
+
+  selectedTestCase = this.testCases[0];
+
   caseAdminInfoExpanded: boolean = false;
   demographicsExpanded: boolean = false;
   circumstancesExpanded: boolean = false;
@@ -44,6 +55,8 @@ export class CaseComparisonContentComponent implements OnInit {
   deathCertificateExpanded: boolean = false;
 
   patientResource: any;
+  actualDocument: any;
+  expectedDocument: any; 
 
   patient: USCorePatientDiff = new USCorePatientDiff( undefined, undefined );
   mdiToEdrs: CompositionMdiToEdrsDiff = new CompositionMdiToEdrsDiff( undefined, undefined );
@@ -56,27 +69,31 @@ export class CaseComparisonContentComponent implements OnInit {
   mannerOfDeath: ObservationMannerOfDeathDiff = new ObservationMannerOfDeathDiff( undefined, undefined );
   practitioner: USCorePractitionerDiff = new USCorePractitionerDiff( undefined, undefined );
 
-  expectedDocument = new ExpectedDocument().value;
-
-  testCases = [
-    {
-      givenName: "Whago",
-      familyName: "Brox"
-    }
-  ]
-
-  _selectedTestCase = this.testCases[0];
-
   constructor(
+    private decedentService: DecedentService,
     private documentHandler: DocumentHandlerService
   ) { }
 
-  ngOnInit(): void {
+  documentBundle$: Observable<any>;
+
+  ngOnInit(): void 
+  {
+    this.decedentService.getDocumentBundle("c926f394-e442-4135-92bc-1e70e8a09b91").subscribe(
+      {next: (documentBundle: any) => {
+        this.expectedDocument = documentBundle;
+        console.log( this.expectedDocument );
+      }
+    });
   }
 
-  selectedTestCase(): string {
-    console.log( this._selectedTestCase );
-    return this._selectedTestCase.givenName + " " + this._selectedTestCase.familyName;
+  selectionChange( event: any )
+  {    
+    this.decedentService.getDocumentBundle(event.value).subscribe(
+      {next: (documentBundle: any) => {
+        this.expectedDocument = documentBundle;
+        this.dodiff();
+      }
+    });
   }
 
   onItemClick( id: any )
@@ -106,110 +123,119 @@ export class CaseComparisonContentComponent implements OnInit {
   deathCertificateStyle = 'invalid';
   deathDateStyle = 'invalid';
 
-  onValueChange(event: Event) {  
-    const value = (event.target as any).value;
+  onValueChange(event: Event) 
+  {  
+    try 
+    {
+      this.actualDocument = JSON.parse( (event.target as any).value );
 
-    if(value) {
-      try {
-        const actualDocument = JSON.parse( value );
+      this.dodiff();
+    } 
+    catch(e) 
+    {
+      console.log(e);
+    }
+  }
 
-        this.mdiToEdrs = new CompositionMdiToEdrsDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Comp_MDItoEDRS ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Comp_MDItoEDRS ));
+  dodiff() 
+  {
+    try {
+      this.mdiToEdrs = new CompositionMdiToEdrsDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Comp_MDItoEDRS ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Comp_MDItoEDRS ));
 
-        this.location = new USCoreLocationDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, USCoreLocation ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, USCoreLocation ));
-        
-        this.tobaccoUse = new ObservationTobaccoUseDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Obs_TobaccoUseContributedToDeath ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_TobaccoUseContributedToDeath ));
+      this.location = new USCoreLocationDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, USCoreLocation ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, USCoreLocation ));
+      
+      this.tobaccoUse = new ObservationTobaccoUseDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Obs_TobaccoUseContributedToDeath ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_TobaccoUseContributedToDeath ));
 
-        this.pregnancy = new ObservationDecedentPregnancyDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Obs_DecedentPregnancy ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_DecedentPregnancy ));
+      this.pregnancy = new ObservationDecedentPregnancyDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Obs_DecedentPregnancy ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_DecedentPregnancy ));
 
-        this.deathDate = new ObservationDeathDateDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Obs_DeathDate ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_DeathDate ));
+      this.deathDate = new ObservationDeathDateDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Obs_DeathDate ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_DeathDate ));
 
-        this.causeOfDeath1List = [];
-        let actualCauseOfDeath1List = this.documentHandler.findResourcesByProfileName( actualDocument, Obs_CauseOfDeathPart1 );
-        let expectedCauseOfDeath1List = this.documentHandler.findResourcesByProfileName( this.expectedDocument, Obs_CauseOfDeathPart1 );
+      this.causeOfDeath1List = [];
+      let actualCauseOfDeath1List = this.documentHandler.findResourcesByProfileName( this.actualDocument, Obs_CauseOfDeathPart1 );
+      let expectedCauseOfDeath1List = this.documentHandler.findResourcesByProfileName( this.expectedDocument, Obs_CauseOfDeathPart1 );
 
-        for (let i = 0; i < actualCauseOfDeath1List.length; i++) {
-          let causeOfDeath1 = new ObservationCauseOfDeathPart1Diff( actualCauseOfDeath1List[i], expectedCauseOfDeath1List[i] );
-          this.causeOfDeath1List.push( causeOfDeath1 );
-        }
-
-        this.causeOfDeath2 = new ObservationCauseOfDeathPart2Diff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Obs_CauseOfDeathPart2 ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_CauseOfDeathPart2 ));
-
-        this.mannerOfDeath = new ObservationMannerOfDeathDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, Obs_MannerOfDeath ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_MannerOfDeath ));
-        
-        this.patient = new USCorePatientDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, USCorePatient ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, USCorePatient ));  
-
-        this.practitioner = new USCorePractitionerDiff(
-          this.documentHandler.findResourceByProfileName( actualDocument, USCorePractitioner ),
-          this.documentHandler.findResourceByProfileName( this.expectedDocument, USCorePractitioner ));
-
-        this.caseAdminInfoStyle = ( 
-          this.mdiToEdrs.extension.style === 'valid' &&
-          this.practitioner.name.style === 'valid' &&
-          this.practitioner.identifier.style === 'valid' &&
-          this.practitioner.telecom.style === 'valid' &&
-          this.practitioner.address.style === 'valid'
-        ) ? 'valid' : 'invalid';
-
-        this.demographicsStyle = ( 
-          this.patient.gender.style === 'valid' &&
-          this.patient.birthDate.style === 'valid' &&
-          this.patient.extension.style === 'valid' &&
-          this.patient.address.style === 'valid'
-        ) ? 'valid' : 'invalid';
-
-        this.circumstancesStyle = ( 
-          this.location.address.style === 'valid' &&
-          this.tobaccoUse.valueCodeableConcept.style === 'valid' &&
-          this.pregnancy.valueCodeableConcept.style === 'valid'
-        ) ? 'valid' : 'invalid';
-
-        this.jurisdictionStyle = ( 
-          this.deathDate.effectiveDateTime.style === 'valid' 
-        ) ? 'valid' : 'invalid';
-
-        this.causeAndMannerStyle = 'valid';
-
-        for (let i = 0; i < actualCauseOfDeath1List.length; i++) {
-          let causeOfDeath1 = new ObservationCauseOfDeathPart1Diff( actualCauseOfDeath1List[i], expectedCauseOfDeath1List[i] );
-          if (causeOfDeath1.valueCodeableConcept.style === 'invalid')
-          {
-            this.causeAndMannerStyle = 'invalid';
-          }
-          if (causeOfDeath1.component.style === 'invalid')
-          {
-            this.causeAndMannerStyle = 'invalid';
-          }
-        }
-
-        if (this.causeOfDeath2.valueCodeableConcept.style === 'invalid' )
-        {
-          this.causeAndMannerStyle = 'invalid';
-        }
-
-        if (this.mannerOfDeath.valueCodeableConcept.style === 'invalid' )
-        {
-          this.causeAndMannerStyle = 'invalid';
-        }
-
-      } catch(e) {
-        console.log(e);
+      for (let i = 0; i < actualCauseOfDeath1List.length; i++) {
+        let causeOfDeath1 = new ObservationCauseOfDeathPart1Diff( actualCauseOfDeath1List[i], expectedCauseOfDeath1List[i] );
+        this.causeOfDeath1List.push( causeOfDeath1 );
       }
+
+      this.causeOfDeath2 = new ObservationCauseOfDeathPart2Diff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Obs_CauseOfDeathPart2 ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_CauseOfDeathPart2 ));
+
+      this.mannerOfDeath = new ObservationMannerOfDeathDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, Obs_MannerOfDeath ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, Obs_MannerOfDeath ));
+      
+      this.patient = new USCorePatientDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, USCorePatient ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, USCorePatient ));  
+
+      this.practitioner = new USCorePractitionerDiff(
+        this.documentHandler.findResourceByProfileName( this.actualDocument, USCorePractitioner ),
+        this.documentHandler.findResourceByProfileName( this.expectedDocument, USCorePractitioner ));
+
+      this.caseAdminInfoStyle = ( 
+        this.mdiToEdrs.extension.style === 'valid' &&
+        this.practitioner.name.style === 'valid' &&
+        this.practitioner.identifier.style === 'valid' &&
+        this.practitioner.telecom.style === 'valid' &&
+        this.practitioner.address.style === 'valid'
+      ) ? 'valid' : 'invalid';
+
+      this.demographicsStyle = ( 
+        this.patient.gender.style === 'valid' &&
+        this.patient.birthDate.style === 'valid' &&
+        this.patient.extension.style === 'valid' &&
+        this.patient.address.style === 'valid'
+      ) ? 'valid' : 'invalid';
+
+      this.circumstancesStyle = ( 
+        this.location.address.style === 'valid' &&
+        this.tobaccoUse.valueCodeableConcept.style === 'valid' &&
+        this.pregnancy.valueCodeableConcept.style === 'valid'
+      ) ? 'valid' : 'invalid';
+
+      this.jurisdictionStyle = ( 
+        this.deathDate.effectiveDateTime.style === 'valid' 
+      ) ? 'valid' : 'invalid';
+
+      this.causeAndMannerStyle = 'valid';
+
+      for (let i = 0; i < actualCauseOfDeath1List.length; i++) {
+        let causeOfDeath1 = new ObservationCauseOfDeathPart1Diff( actualCauseOfDeath1List[i], expectedCauseOfDeath1List[i] );
+        if (causeOfDeath1.valueCodeableConcept.style === 'invalid')
+        {
+          this.causeAndMannerStyle = 'invalid';
+        }
+        if (causeOfDeath1.component.style === 'invalid')
+        {
+          this.causeAndMannerStyle = 'invalid';
+        }
+      }
+
+      if (this.causeOfDeath2.valueCodeableConcept.style === 'invalid' )
+      {
+        this.causeAndMannerStyle = 'invalid';
+      }
+
+      if (this.mannerOfDeath.valueCodeableConcept.style === 'invalid' )
+      {
+        this.causeAndMannerStyle = 'invalid';
+      }
+
+    } catch(e) {
+      console.log(e);
     }
   }
 }
