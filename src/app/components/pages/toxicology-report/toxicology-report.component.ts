78b7d90cb64ee532b6d82be9d49753cb89866cec
Plamen Tassev <plamen@gatech.edu>
2022-11-16 15:19:30 -0500

    Created some structure for the toxicology viewer

---
+++ src/app/components/pages/toxicology-report/toxicology-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toxicology-report',
+  templateUrl: './toxicology-report.component.html',
+  styleUrls: ['./toxicology-report.component.css']
+})
+export class ToxicologyReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
