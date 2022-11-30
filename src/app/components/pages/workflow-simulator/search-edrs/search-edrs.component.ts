8bb41a4581f08f87b52fa65f3978cc4960dcec25
Plamen Tassev <plamen@gatech.edu>
2022-11-30 10:34:03 -0500

    APIMR-278 implemented the stepper as a indicator in view only mode

--- src/app/components/pages/workflow-simulator/search-edrs/search-edrs.component.ts
+++ src/app/components/pages/workflow-simulator/search-edrs/search-edrs.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
 import {SearchEdrsService} from "../../../../service/search-edrs.service";
 
 @Component({
   selector: 'app-search-edrs',
   templateUrl: './search-edrs.component.html',
-  styleUrls: ['./search-edrs.component.css']
+  styleUrls: ['./search-edrs.component.css'],
+  encapsulation: ViewEncapsulation.None // we need this to disable the header for the stepper and use it as an indicator
 })
 export class SearchEdrsComponent implements OnInit {
 
@@ -25,8 +26,7 @@ export class SearchEdrsComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private searchEdrsService: SearchEdrsService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.searchEdrsService.documentBundle$.subscribe({
