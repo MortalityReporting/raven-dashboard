49a3629f8143c371f2d5af44f9dad55415fa646d
Ellie Shivers <elliemshivers@gmail.com>
2022-12-15 10:33:06 -0500

    initial icon work

--- src/app/components/pages/toxicology-report/toxicology-report.component.ts
+++ src/app/components/pages/toxicology-report/toxicology-report.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {ToxicologyHandlerService} from "../../../service/toxicology-handler.service";
 import {ActivatedRoute} from "@angular/router";
 import {Observable} from "rxjs";
+import {FhirResourceProviderService} from "../../../service/fhir-resource-provider.service";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-toxicology-report',
@@ -12,17 +15,39 @@ export class ToxicologyReportComponent implements OnInit {
 
   messageBundle$: Observable<any>;
   toxHeader$: Observable<any>;
+  sidenavExpanded = false;
+  autosize: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private toxicologyHandler: ToxicologyHandlerService,
-  ) { }
+    private fhirResourceProvider: FhirResourceProviderService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    const path = "../../assets"
+    this.matIconRegistry.addSvgIcon("labs", this.domSanitizer
+      .bypassSecurityTrustResourceUrl(`${path}/labs.svg`));
+  }
 
   ngOnInit(): void {
     let toxLabId = this.route.snapshot.params['id'];
     this.messageBundle$ = this.toxicologyHandler.getMessageBundle(toxLabId);
-    this.messageBundle$.subscribe(bundle => console.log(bundle));
+    this.messageBundle$.subscribe(bundle => {
+      this.fhirResourceProvider.setSelectedFhirResource(bundle);
+      }
+    );
     this.toxHeader$ = this.toxicologyHandler.toxHeader$;
+
   }
 
+  onSidenavResize(expanded: boolean) {
+    this.sidenavExpanded = expanded;
+    this.autosize = true;
+    setTimeout(() => this.autosize = false, 1);
+  }
+
+  onItemClick(id: string) {
+    //this.caseSummaryContentComponent.onSetState(id, true);
+  }
 }
