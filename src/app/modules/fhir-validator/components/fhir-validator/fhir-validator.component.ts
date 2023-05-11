003b0eb0f0b53e11b6d609946d7a1da08a1247b7
Plamen Tassev <plamen@gatech.edu>
2023-05-11 10:09:47 -0400

    Fhir validator style change

--- src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
+++ src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {ValidatorInput} from "ngx-fhir-validator";
+import {ModuleHeaderConfig} from "../../../../providers/module-header-config";
 
 @Component({
   selector: 'app-fhir-validator',
@@ -7,6 +8,10 @@ import {ValidatorInput} from "ngx-fhir-validator";
   styleUrls: ['./fhir-validator.component.scss']
 })
 export class FhirValidatorComponent {
+  constructor(
+    @Inject('fhirValidatorConfig') public config: ModuleHeaderConfig) {
+  }
+
 
   validationTextFormat: ValidatorInput = {format: 'xml and json', accepts: 'text/*,.xml,.json'};
 
