32062e03279a04244c5c1a7231a814b5945c12b8
Ellie Shivers <elliemshivers@gmail.com>
2023-08-04 14:35:27 -0400

    Config refactoring to allow overrides

--- src/app/service/config.service.ts
+++ src/app/service/config.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import {Config} from "../model/config";
 import packageInfo from '../../../package.json';
-import {HttpBackend, HttpClient} from "@angular/common/http";
+import {HttpBackend, HttpClient, HttpRequest} from "@angular/common/http";
 import {map, of} from "rxjs";
 import {catchError} from "rxjs/operators";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
+  defaultConfigPath: string = "https://raven.heat.icl.gtri.org/raven-dashboard-api/config"
+  defaultLocalConfigPath = '../../assets/config/config.json'
   config: Config = new Config();
   packageInfo = packageInfo;
   private http: HttpClient
@@ -19,7 +22,20 @@ export class ConfigService {
   }
 
   loadConfig() {
-    return this.http.get<Config>('../../assets/config/config.json').pipe(
+    let useLocalConfig = false;
+    let configPath = this.defaultConfigPath;
+    if (environment) {
+      if (environment?.useLocalConfig) {
+        useLocalConfig = true;
+      }
+      if (environment?.overrideConfigLocation?.trim()) {
+        configPath = environment?.overrideConfigLocation?.trim()
+      }
+      else if (useLocalConfig) {
+        configPath = this.defaultLocalConfigPath;
+      }
+    }
+    return this.http.get<Config>(configPath).pipe(
       map(config => {
         config.version = "v" + this.packageInfo.version;
         this.config = config;
