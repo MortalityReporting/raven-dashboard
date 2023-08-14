6569deb19b58e5c70c88632ac18a2228f5ee0cdd
Plamen Tassev <plamen@gatech.edu>
2023-08-14 15:48:38 -0400

    minor fix to the css and stages

--- src/app/modules/workflow-simulator/components/tests/onboarding/onboarding.component.ts
+++ src/app/modules/workflow-simulator/components/tests/onboarding/onboarding.component.ts
@@ -1,21 +1,26 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {LogLine} from "../../../../../../../projects/ngx-hisb-logger/src/lib/modal/log-line";
-import {LoggerService} from "../../../../../../../projects/ngx-hisb-logger/src/lib/services/logger.service";
 import {MatAccordion} from "@angular/material/expansion";
+import {LoggerService} from "ngx-hisb-logger";
+
+export interface Stage {
+  index: number;
+  expanded: boolean;
+}
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.css']
 })
-export class OnboardingComponent implements OnInit, AfterViewInit{
+export class OnboardingComponent implements OnInit {
   @ViewChild(MatAccordion) accordion: MatAccordion;
   constructor(
     private log: LoggerService,
   ){}
 
   loggerData: LogLine[];
-  componentCounterArray: number[] = [0];
+  stageList: Stage[] =[];
 
   clearLog(){
     this.log.clear()
@@ -23,23 +28,19 @@ export class OnboardingComponent implements OnInit, AfterViewInit{
 
   ngOnInit(): void {
     this.log.logStream$.subscribe(value=> this.loggerData = value);
+    this.addStage();
   }
 
   addStage() {
-    this.componentCounterArray.push(this.componentCounterArray.length -1);
+    this.stageList.push({index: this.stageList.length+ 1, expanded: true})
   }
 
   removeComponent(index: number, event?: any) {
-    this.componentCounterArray.splice(index, 1);
-  }
-
-  ngAfterViewInit(): void {
-    setTimeout(()=> {
-      this.accordion.openAll();
-    });
+    this.stageList.splice(index, 1);
   }
 
   getStageIndex(i: number) {
     return i+1;
   }
+
 }
