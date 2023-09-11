60dd228d2b14e995bdccbaa4b1610b701da89e5e
Plamen Tassev <plamen@gatech.edu>
2023-09-11 15:00:14 -0400

    Implemented record import from csv file

--- src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
+++ src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject} from '@angular/core';
 import {ValidatorInput} from "ngx-fhir-validator";
 import {ModuleHeaderConfig} from "../../../../providers/module-header-config";
+import autoTable from 'jspdf-autotable'
+import jsPDF from "jspdf";
 
 @Component({
   selector: 'app-fhir-validator',
@@ -12,7 +14,41 @@ export class FhirValidatorComponent {
     @Inject('fhirValidatorConfig') public config: ModuleHeaderConfig) {
   }
 
+  exportToPDF(data: any): void {
+    const doc: jsPDF = new jsPDF();
+    let date = new Date().toUTCString();
+    const reportTitle = "FHIR Validator Results";
+    doc.setFontSize(6);
+    doc.setTextColor(119, 119, 119);
+    doc.text(date, 160, 12);
+    doc.setFontSize(12);
+    doc.setTextColor(50, 50, 50);
+    doc.text(reportTitle, 14, 12);
+    autoTable(doc, {
+      columnStyles: {
+        severity: {halign: 'left'},
+        diagnostics: {halign: 'left'},
+        location: {halign: 'left', cellWidth: 25}
+      }, // European countries centered
+      body: data,
+      columns: [
+        {header: 'Severity', dataKey: 'severity'},
+        {header: 'Diagnostics', dataKey: 'diagnostics'},
+        {header: 'Location', dataKey: 'location'},
+      ],
+      styles: {fontSize: 8, fontStyle: "normal"},
+    });
+    try {
+      doc.save(`${reportTitle}_${date}.pdf`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   validationTextFormat: ValidatorInput = {format: 'xml and json', accepts: 'text/*,.xml,.json'};
+
+  onExportToPdf(data: any) {
+    this.exportToPDF(data);
+  }
 
 }
