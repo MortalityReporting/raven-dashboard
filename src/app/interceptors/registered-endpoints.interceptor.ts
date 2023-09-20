012357baaedd885bed198943ab4ec6dca7d3e6c3
Plamen Tassev <plamen@gatech.edu>
2023-09-20 16:02:24 -0400

    Exort FHIR validator results to zip

--- src/app/interceptors/registered-endpoints.interceptor.ts
+++ src/app/interceptors/registered-endpoints.interceptor.ts
@@ -14,7 +14,7 @@ export class RegisteredEndpointsInterceptor implements HttpInterceptor {
   config: Config;
   registeredEndpoints: any[];
   ignoreList = ["assets/", "../assets/", "../../assets/"];
-  skipInterceptor = false;
+  skipInterceptor = true;
 
   constructor(private configService: ConfigService) {
     this.config = configService.config;
@@ -42,26 +42,26 @@ export class RegisteredEndpointsInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    // if (this.skipInterceptor) {
+    if (this.skipInterceptor) {
       return next.handle(request);
-    // }
-    // let service = this.registeredEndpoints.find(service => request.url.startsWith(service.baseUrl));
-    // if (this.ignoreList.some(item => request.url.startsWith(item))) {
-    //   return next.handle(request);
-    // }
-    // else if (!service) {
-    //   console.info(`${request.url} is not registered with the application (nor on the ignore list).`)
-    //   return next.handle(request);
-    // }
-    // else {
-    //   let service = this.registeredEndpoints.find(service => request.url.startsWith(service.baseUrl));
-    //   if (service?.allowedEndpoints?.some(endpoint => request.url === service.baseUrl + endpoint || endpoint === "*")) {
-    //     return next.handle(request);
-    //   }
-    //   else {
-    //     return throwError(() => new Error(`${request.url} does not match a registered URL.`));
-    //   }
-    // }
+    }
+    let service = this.registeredEndpoints.find(service => request.url.startsWith(service.baseUrl));
+    if (this.ignoreList.some(item => request.url.startsWith(item))) {
+      return next.handle(request);
+    }
+    else if (!service) {
+      console.info(`${request.url} is not registered with the application (nor on the ignore list).`)
+      return next.handle(request);
+    }
+    else {
+      let service = this.registeredEndpoints.find(service => request.url.startsWith(service.baseUrl));
+      if (service?.allowedEndpoints?.some(endpoint => request.url === service.baseUrl + endpoint || endpoint === "*")) {
+        return next.handle(request);
+      }
+      else {
+        return throwError(() => new Error(`${request.url} does not match a registered URL.`));
+      }
+    }
   }
 
 }
