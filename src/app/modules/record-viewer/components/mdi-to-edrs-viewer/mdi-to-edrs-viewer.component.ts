5adc10c9395030f6ca14f999bbc3f6874e167845
Plamen Tassev <plamen@gatech.edu>
2023-10-11 14:34:53 -0400

    Added error handling

--- src/app/modules/record-viewer/components/mdi-to-edrs-viewer/mdi-to-edrs-viewer.component.ts
+++ src/app/modules/record-viewer/components/mdi-to-edrs-viewer/mdi-to-edrs-viewer.component.ts
@@ -1,5 +1,4 @@
 import {Component, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Observable} from "rxjs";
 import {ActivatedRoute} from "@angular/router";
 import {MdiToEdrsDocumentHandlerService} from "../../services/mdi-to-edrs-document-handler.service";
 import {ModuleHeaderConfig} from "../../../../providers/module-header-config";
@@ -7,6 +6,7 @@ import {AppConfiguration} from "../../../../providers/app-configuration";
 import {MdiToEdrsViewerContentComponent} from "./mdi-to-edrs-viewer-content/mdi-to-edrs-viewer-content.component";
 import {MdiToEdrsRecord} from "../../models/mdiToEdrsRecord";
 import {FhirExplorerService} from "../../../fhir-explorer/services/fhir-explorer.service";
+import {UtilsService} from "../../../../service/utils.service";
 
 @Component({
   selector: 'record-viewer-mdi-to-edrs-viewer',
@@ -18,6 +18,7 @@ export class MdiToEdrsViewerComponent implements OnInit, OnDestroy {
 
   /** Inputs to children **/
   mdiToEdrsRecord: MdiToEdrsRecord;
+  isLoading: boolean = false;
 
   documentBundle: any = {};
   compositionId: string = "";
@@ -33,16 +34,24 @@ export class MdiToEdrsViewerComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     public documentHandler: MdiToEdrsDocumentHandlerService,
     private fhirExplorerService: FhirExplorerService,
+    private utilsService: UtilsService,
     @Inject('config') public config: ModuleHeaderConfig,
     @Inject('appConfig') public appConfig: AppConfiguration
   ) { }
 
   ngOnInit(): void {
     let subjectId = this.route.snapshot.params['id'];
+    this.isLoading = true;
     this.documentHandler.getRecord(subjectId).subscribe({
       next: record => {
         this.fhirExplorerService.setSelectedFhirResource(record.documentBundle);
         this.mdiToEdrsRecord = record;
+        this.isLoading = false;
+      },
+      error: err => {
+        console.error(err);
+        this.utilsService.showErrorMessage("Server error loading records.");
+        this.isLoading = false;
       }
     });
   }
