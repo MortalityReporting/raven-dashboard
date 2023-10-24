d0d1a9bf47603020ca87eaca6fab6aeec7bc4373
Plamen Tassev <plamen@gatech.edu>
2023-10-24 10:36:36 -0400

    Restoring env file, was committed by accident

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,6 @@
 
+// To create a build for a local http server, replace the contents of this file with the contents of environments.ts.
+// The prod file uses sensitive info stored in a different github repo
 export const environment = {
-  // API and Configuration Happens at build time as core dependency.
-  dashboardApi: "https://raven.dev.heat.icl.gtri.org/raven-dashboard-api/",
-  //dashboardApi: "http://127.0.0.1:8000/",
-  audience: "https://raven.dev.heat.icl.gtri.org/raven-dashboard-api/",
-  domain: "dev-dk7cyfpkwowbtdbt.us.auth0.com",
-  clientId: "M7knIi1ioWMc6Lufbt5lbyTrnxpKmL4q",
-  useLocalConfig: false,
-  overrideConfigLocation: "",
-
-  // TODO: Move to runtime config services.
-  // ravenFhirServer: "https://raven.dev.heat.icl.gtri.org/mdi-fhir-server/fhir",
-  // ravenFhirServerBasicAuth: "client:secret",
-  // ravenImportApi: "https://raven.dev.heat.icl.gtri.org/raven-import-api",
-  fhirValidator: "https://dev.heat.icl.gtri.org/fhir-validator-service/fhir", // TODO: Refactor modules to not rely on this.
-  // blueJayServerBase: "https://bluejay.heat.icl.gtri.org/mdi-fhir-server",
-  adminRedirectUrl: "https://localhost:4200/admin-panel",
-  adminLogoutUrl: "https://localhost:4200/"
+  production: true
 };
