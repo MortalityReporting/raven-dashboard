186bdd85cb84b64da8fb99a5e6711dc583e9e031
Plamen Tassev <plamen@gatech.edu>
2024-01-19 12:10:57 -0500

    Quick NPE fix

--- src/app/modules/testing-events/components/registered-module/registered-module.component.ts
+++ src/app/modules/testing-events/components/registered-module/registered-module.component.ts
@@ -50,7 +50,7 @@ export class RegisteredModuleComponent implements OnInit{
           displayItem.linkId = eventItem.linkId; // Test LinkId
           displayItem.testCode = eventItem.code; // Code used to determine test loaded
           const qrItem = this.currentRegistration.item.find((qrItem: QuestionnaireResponseItem) => qrItem.linkId === eventItem.linkId);
-          displayItem.testStatus = qrItem.answer[0].valueCoding.code; // Test Status
+          displayItem.testStatus = qrItem?.answer?.[0]?.valueCoding?.code; // Test Status
           displayItem.description = eventItem.description;
           registrationDisplay.items.push(displayItem);
         });
