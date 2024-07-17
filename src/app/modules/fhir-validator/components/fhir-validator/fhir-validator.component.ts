2cff09eed502fcd445fcfe91e518dc708282deee
Plamen Tassev <plamen@gatech.edu>
2024-07-17 10:21:53 -0400

    APIMR-629: Implemented a solution for FHIR validator data driven ig

--- src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
+++ src/app/modules/fhir-validator/components/fhir-validator/fhir-validator.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject} from '@angular/core';
-import {ImplementationGuide, ValidatorInput} from "ngx-fhir-validator";
+import {ValidatorInput} from "ngx-fhir-validator";
 import {ModuleHeaderConfig} from "../../../../providers/module-header-config";
 import {FhirValidatorResultsExportService} from "../../../../service/fhir-validator-results-export.service";
 
@@ -9,24 +9,6 @@ import {FhirValidatorResultsExportService} from "../../../../service/fhir-valida
   styleUrls: ['./fhir-validator.component.scss']
 })
 export class FhirValidatorComponent {
-  igList: ImplementationGuide[] = [
-    {
-      "name": "mdi#1.1.0",
-      "valueString": "hl7.fhir.us.mdi#1.1.0",
-      "display": "MDI FHIR IG - 1.1.0"
-    },
-    {
-      "name": "mdi#current",
-      "valueString": "hl7.fhir.us.mdi#current",
-      "display": "MDI FHIR IG - Current"
-    },
-    {
-      "name": "vrdr#2.2.0",
-      "valueString": "hl7.fhir.us.vrdr#2.2.0",
-      "display": "VRDR FHIR IG - 2.2.0"
-    }
-  ];
-
   constructor(
     @Inject('fhirValidatorConfig') public config: ModuleHeaderConfig,
     private fhirValidatorResultsExportService: FhirValidatorResultsExportService) {
