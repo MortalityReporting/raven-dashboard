b61856d4f7e4c407a1e2a29c3e43100b4523a545
ptassev3 <plamentassev@gmail.com>
2025-04-16 09:37:26 -0400

    minor refactoring to the signature block

--- src/app/modules/record-viewer/components/dcr-viewer/dcr-viewer-content/dcr-viewer-signature/dcr-viewer-signature.component.ts
+++ src/app/modules/record-viewer/components/dcr-viewer/dcr-viewer-content/dcr-viewer-signature/dcr-viewer-signature.component.ts
@@ -1,11 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component, input} from '@angular/core';
+import {SignatureBlock} from "../../../../models/dcr-record";
 
 @Component({
   selector: 'app-dcr-viewer-signature',
   standalone: false,
   templateUrl: './dcr-viewer-signature.component.html',
-  styleUrl: './dcr-viewer-signature.component.css'
+  styleUrls: ['./dcr-viewer-signature.component.css',
+    '../../../mdi-to-edrs-viewer/mdi-to-edrs-viewer.component.scss']
 })
 export class DcrViewerSignatureComponent {
-  @Input() signature!: any;
+  signature = input<SignatureBlock>()
 }
